0950b85b80d9cd0d94da874b36bc794b4c75f6f6
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-06-23 15:30:58 +0700

    jika FRONT_position > REAR_position iterasi dari FRONT hingga akhir array

--- circullarQueues/circullarQueues.cpp
+++ circullarQueues/circullarQueues.cpp
@@ -76,8 +76,18 @@ public:
 		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
-
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
 			}
+			cout << endl;
 		}
-	}
-};
+		else {
+			//jika FRONT_position > REAR_position iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			
